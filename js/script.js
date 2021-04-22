7b5555aef0da5ba7bc4576cad78f6fb47490f780
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-04-22 16:05:48 +0200

    Aggiunto v for a menu footer e aggiunte alcune var

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ let app = new Vue(
       navMenu: [
         'Home', 'Shop', 'About', 'Blog', 'Contact', 'Shop my brand'
       ],
+      shopByBrand: [
+        'Bed', 'Food', 'Toys', 'Transport'
+      ],
+      usefulLinks: [
+        'My account', 'Orders', 'Checkout', 'Cart'
+      ],
+      menuBottom: [
+        'Home', 'About', 'Blog', 'Contact', 'Shop'
+      ],
     }
   }
 );
